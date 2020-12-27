375536842c2e38840d294302d744c0917dce1f50
hoangpham <58016689+hoangpham-brainstech@users.noreply.github.com>
2020-12-28 00:34:14 +0900

    added problem 66
    Signed-off-by: hoangpham <58016689+hoangpham-brainstech@users.noreply.github.com>

--- leetcode/64_minimum_path_sum.py
+++ leetcode/64_minimum_path_sum.py
@@ -16,10 +16,12 @@ class Solution:
                 dp[i][0] = dp[i-1][0] + grid[i][0]
 
         elif n > 1 and m > 1:
+            # Handle boundary cases
             for i in range(1, n):
                 dp[0][i] = dp[0][i-1] + grid[0][i]
             for i in range(1, m):
                 dp[i][0] = dp[i-1][0] + grid[i][0]
+            # DP
             for i in range(1, m):
                 for j in range(1,n):
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
---
+++ leetcode/66_plus_one.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digitsLength = len(digits)
+        for i in range(digitsLength - 1, -1, -1):
+            if (digits[i] < 9):
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        newDigits = [0 for _ in range(digitsLength+1)]
+        newDigits[0] = 1
+        return newDigits
+        
+if __name__=="__main__":
+    digits = [9]
+    solution = Solution()
+    print(solution.plusOne(digits))
