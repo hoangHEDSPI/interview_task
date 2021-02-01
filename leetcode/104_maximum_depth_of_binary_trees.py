9ea71db8119754704aa211da9e5790a526e5d1d8
hoangpham <58016689+hoangpham-brainstech@users.noreply.github.com>
2021-02-01 23:33:43 +0900

    added problem 104
    Signed-off-by: hoangpham <58016689+hoangpham-brainstech@users.noreply.github.com>

---
+++ leetcode/104_maximum_depth_of_binary_trees.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        depth = 0
+        level = [root] if root else []
+        while level:
+            depth += 1
+            queue = []
+            for el in level:
+                if el.left:
+                    queue.append(el.left)
+                if el.right:
+                    queue.append(el.right)
+            level = queue
+        return depth
---
+++ leetcode/85_maximal_rectangle.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        pass
