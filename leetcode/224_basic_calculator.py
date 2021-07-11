9ffefcf56e5c71192b3fd4b0ffb5e024cbcd29f5
Hoang Pham <a2k40pbc@gmail.com>
2021-07-11 21:54:18 +0900

    Create 224_basic_calculator.py

---
+++ leetcode/224_basic_calculator.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        # default sign value is +1 (sign can get the value of -1/+1)
+        # num will store the current considering number
+        # res will store the result
+        
+        num, sign, res = 0, 1, 0
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = num*10 + int(c)
+            elif c in '+-':
+                res += num*sign
+                sign = -1 if c == '-' else 1
+                num = 0
+            elif c == '(':
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ')':
+                res += sign*num
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+        return res + num*sign
+    
