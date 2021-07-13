96f047b5dbc321a60331f62e065886576d7e21f7
Hoang Pham <a2k40pbc@gmail.com>
2021-07-14 00:01:22 +0900

    Update 40 (faster solution)

--- leetcode/40_combination_sum_II.py
+++ leetcode/40_combination_sum_II.py
@@ -1,6 +1,5 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        
         def backtrack(combination, curr, remain, results):
             if remain == 0:
                 results.append(list(combination))
@@ -11,12 +10,8 @@ class Solution:
                 picked = candidates[next_curr]
                 if remain - picked < 0:
                     break
-                combination.append(picked)
-                backtrack(combination, next_curr+1, remain - picked, results)
-                
-                combination.pop()
+                backtrack(combination + [candidates[next_curr]], next_curr+1, remain - picked, results)
         candidates.sort()
         comb, res = [], []
         backtrack(comb, 0, target, res)
         return res
-      
